6e6559896cc70b86c4b6033f48f3b6ad1fc58051
lidongdong <964991296@qq.com>
2016-12-22 14:33:39 +0800

    Initial Commit

---
+++ PaySDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaySDK
+//
+//  Created by 李冬冬 on 2016/12/22.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaySDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaySDK
+//
+//  Created by 李冬冬 on 2016/12/22.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
